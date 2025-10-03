10b4d6391cc40d45febd617232eff92383b6906f
Nicolas Luis Moura de Almeida <nicolasluis215@gmail.com>
2025-10-03 11:04:05 -0300

    Commit - 03/10/2025

--- src/main/java/com/senai/conta_bancaria/domain/exceptions/TipoDeContaInvalidaException.java
+++ src/main/java/com/senai/conta_bancaria/domain/exceptions/TipoDeContaInvalidaException.java
@@ -2,6 +2,6 @@ package com.senai.conta_bancaria.domain.exceptions;
 
 public class TipoDeContaInvalidaException extends RuntimeException {
     public TipoDeContaInvalidaException() {
-        super("Tipo de conta inválida. Os tipos válidos são: 'corrente' ou 'poupança'.");
+        super("Tipo de conta inválida. Os tipos válidos são: 'CORRENTE' ou 'POUPANCA'.");
     }
 }
